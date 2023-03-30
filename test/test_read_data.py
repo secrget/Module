b6b75af9b12f6116a055c68e9e97781583b39299
Yegor <mamitkoegorb@gmail.com>
2023-03-30 14:44:04 +0300

    unit commit

---
+++ test/test_read_data.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+from code import main
+
+def test_read_data():
+    expected = {
+        'Ukraine': {'2000': 48000000, '2001': 48100000, '2002': 48200000},
+        'Poland': {'2000': 38000000, '2001': 38100000, '2002': 38200000},
+        'Germany': {'2000': 82000000, '2001': 82100000, '2002': 82200000}
+    }
+    actual = read_data('population.txt')
+    assert actual == expected
